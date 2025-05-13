30b3b76afcc78ddf1eb47916c234fc7e9ec858da
Averie <averiegaspar@gmail.com>
2025-05-13 16:10:15 +0800

    "DELETE CONTENT"

--- CEGMS/server/models/CenteredTable/Counter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const CounterSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  seq: { type: Number, default: 0 },
-});
-
-const CounterModel = mongoose.model("Counter", CounterSchema);
-module.exports = CounterModel;
